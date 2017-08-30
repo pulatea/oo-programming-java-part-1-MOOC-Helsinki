e76751d1aca5581417081ce865f7cdbfb48a99a4
pulatea <teaapulaa@gmail.com>
2017-08-30 23:02:24 +0200

    Create 010.Circumreference.java

---
+++ 010.Circumreference.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Circumference {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        // Program your solution here 
+        
+        System.out.print("Type of the radius: ");
+        int radius = Integer.parseInt(reader.nextLine());
+        
+        System.out.println("Circumreference of the circle: " +
+                2 * Math.PI * radius);
+    }
+}
