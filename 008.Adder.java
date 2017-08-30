773880a4b2ad65370f775b6d44e1d4651b575d84
pulatea <teaapulaa@gmail.com>
2017-08-30 22:59:08 +0200

    Create 008.Adder.java

---
+++ 008.Adder.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Adder {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        // Implement your program here. Remember to ask the input from user
+        
+        System.out.print("Type a number: ");
+        int number = Integer.parseInt(reader.nextLine());
+        
+        System.out.print("Type another number: ");
+        int otherNumber = Integer.parseInt(reader.nextLine());
+       
+        System.out.println("Sum of the numbers: " + (number + otherNumber));
+    }
+}
