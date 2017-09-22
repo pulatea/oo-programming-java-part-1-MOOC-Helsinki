659ea07fecb0f0f5ca0a72c24ebfd8c8480ba2c0
pulatea <teaapulaa@gmail.com>
2017-09-22 15:43:19 +0200

    Create 059.WordsInReverseOrder.java

---
+++ 059.WordsInReverseOrder.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class WordsInReverseOrder {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        // create here an ArrayList
+        
+        ArrayList<String> words = new ArrayList<String>();
+        
+        String word = "";
+        
+        while(true){
+            System.out.print("Type a word: ");
+            word = reader.nextLine();
+            words.add(word);
+            
+            if(word.isEmpty())
+                break;
+        }
+        
+        Collections.reverse(words);
+        System.out.println("You typed the following words: ");
+        for(String allWords : words){
+            System.out.println(allWords);
+        }
+    }
+}
