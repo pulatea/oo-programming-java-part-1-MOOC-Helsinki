49e16310ac79c5a8fa90b2cb9bdf1a96853af917
pulatea <teaapulaa@gmail.com>
2017-09-22 15:48:35 +0200

    Create 067.Variance.java

---
+++ 067.Variance.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Variance {
+    // Copy here sum from exercise 63 
+    public static int sum(ArrayList<Integer> list) {
+        
+        int sum = 0;
+        for(int i = 0; i < list.size(); i++){
+            sum += list.get(i);
+        }
+        return sum;
+    }
+    
+    // Copy here average from exercise 64 
+    public static double average(ArrayList<Integer> list) {
+        return sum(list) / (double) list.size();
+    }
+
+    public static double variance(ArrayList<Integer> list) {
+        // write code here
+        double average = average(list);
+        double sum = 0;
+        
+        for(int i = 0; i < list.size(); i++){
+            sum += Math.pow((double)list.get(i) - average, 2);
+        }
+       
+        double variance = sum / (list.size() - 1);
+        
+        return variance;
+    }
+    
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(3);
+        list.add(2);
+        list.add(7);
+        list.add(2);
+        
+        System.out.println("The variance is: " + variance(list));
+    }
+
+}
